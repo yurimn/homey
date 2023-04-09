09a5a7a0008846165acfb89eba673914a9727dfe
yurimn <leeyou6757@gmail.com>
2023-04-10 00:05:40 +0900

    edit

--- src/Description.js
+++ src/Description.js
@@ -4,6 +4,9 @@ function Description() {
 	return (
 		<div className="Description-page">
 			<h1>Release</h1>
+			<a href={`${process.env.PUBLIC_URL}/homey1.0.1.apk`} download>
+				Homey version 1.0.1
+			</a>
 			<a href={`${process.env.PUBLIC_URL}/homey1.0.0.apk`} download>
 				Homey version 1.0.0
 			</a>
