96f9a4f31d44bb70b6f133a5a4eff3a1107d18ff
yurimn <leeyou6757@gmail.com>
2023-04-10 00:00:55 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Release, Description, Error } from ".";
+import { Description, Error } from ".";
 
 function App() {
 	return (
